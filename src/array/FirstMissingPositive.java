649e91d99ec7d0f4b1a1896a41f7c689ea81870c
zhihui_wu01@163.com <1449553037@qq.com>
2020-09-17 19:54:27 +0800

    FEAT 缺失的第一个正数，置换法

--- src/array/FirstMissingPositive.java
+++ src/array/FirstMissingPositive.java
@@ -13,10 +13,10 @@ import java.util.Set;
 public class FirstMissingPositive {
     public static void main(String[] args) {
         FirstMissingPositive firstMissingPositive = new FirstMissingPositive();
-//        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
 //        int[] nums = new int[]{0, 0, 0};
 //        int[] nums = new int[]{3, 4, -1, 1};
-        int[] nums = new int[]{1};
+//        int[] nums = new int[]{1};
 
         // 解法一：常规做法-哈希表
 //        firstMissingPositive.firstMissingPositive(nums);
@@ -54,17 +54,20 @@ public class FirstMissingPositive {
      * 空间复杂度：O（1）
      */
     public int firstMissingPositive1(int[] nums) {
+        // 将负数元素值换成元素个数加一
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] <= 0) {
                 nums[i] = nums.length + 1;
             }
         }
+        // 将所有在（1 ~ N-1）范围内的元素，其元素值-1作为下标，定位元素并取负
         for (int i = 0; i < nums.length; i++) {
             int num = Math.abs(nums[i]);
             if (num <= nums.length) {
                 nums[num - 1] = -Math.abs(nums[num - 1]);
             }
         }
+        // 第一个非负的元素记为缺失的正数
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
                 System.out.println(i + 1);
@@ -80,6 +83,23 @@ public class FirstMissingPositive {
      * 空间复杂度：O（1）
      */
     public int firstMissingPositive2(int[] nums) {
-        return 1;
+        // 修正数组的顺序，将值为X的元素交换到下标为（x-1）的位置
+        int size = nums.length;
+        for (int i = 0; i < size; i++) {
+            while (nums[i] > 0 && nums[i] < size && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        // 判断第一个出现元素值不等于下标加一的位置，即可
+        for (int i = 0; i < size; i++) {
+            if (nums[i] - 1 != i) {
+                System.out.println(i + 1);
+                return i + 1;
+            }
+        }
+        System.out.println(size + 1);
+        return size + 1;
     }
 }
