0806d92e4b92a9a8fc50681024c2c7067ec9943f
zhihui_wu01@163.com <1449553037@qq.com>
2020-09-18 09:32:04 +0800

    FEAT 237. 删除链表中的节点

--- src/linklist/DeleteNodeInALinkedList.java
+++ src/linklist/DeleteNodeInALinkedList.java
@@ -7,4 +7,44 @@ package linklist;
  * https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
  */
 public class DeleteNodeInALinkedList {
+
+    public static void main(String[] args) {
+        DeleteNodeInALinkedList deleteNodeInALinkedList = new DeleteNodeInALinkedList();
+        ListNode node1 = new ListNode(4);
+        ListNode node2 = new ListNode(5);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(9);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = null;
+
+        deleteNodeInALinkedList.print(node1);
+
+        deleteNodeInALinkedList.deleteNode(node2);
+
+        deleteNodeInALinkedList.print(node1);
+
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public void print(ListNode node) {
+        ListNode temp = node;
+        while (temp != null) {
+            System.out.print(temp.val);
+            temp = temp.next;
+        }
+        System.out.println("");
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
