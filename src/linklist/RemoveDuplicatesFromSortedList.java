31a32477fd8202277d72e9e3a8702867abe1e1ff
zhihui_wu01@163.com <1449553037@qq.com>
2020-10-18 14:49:23 +0800

    FEAT 83. 删除排序链表中的重复元素

--- src/linklist/RemoveDuplicatesFromSortedList.java
+++ src/linklist/RemoveDuplicatesFromSortedList.java
@@ -7,4 +7,47 @@ package linklist;
  * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
  */
 public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedList removeDuplicatesFromSortedList = new RemoveDuplicatesFromSortedList();
+
+        ListNode list1 = new ListNode(1);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(2);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(3);
+        list1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = null;
+
+
+        // 解法一：快慢指针，反转一半链表
+        ListNode result = removeDuplicatesFromSortedList.deleteDuplicates(list1);
+
+        LinkedListUtils.print(result);
+    }
+
+    /**
+     * 直接遍历处理
+     * 时间复杂度：O（ N ）
+     * 空间复杂度：O（ 1 ）
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null) {
+            if (fast.val == slow.val) {
+                fast = fast.next;
+                slow.next = fast;
+            }else {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        return head;
+    }
 }
