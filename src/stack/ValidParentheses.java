1ea86198a55bf3499314095f6cedf5e5e64ab428
zhihui_wu01@163.com <1449553037@qq.com>
2020-10-18 16:33:47 +0800

    FEAT 20. 有效的括号

--- src/stack/ValidParentheses.java
+++ src/stack/ValidParentheses.java
@@ -1,5 +1,9 @@
 package stack;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @Description: 20. 有效的括号
  * @Author: 吴智慧
@@ -7,4 +11,44 @@ package stack;
  * https://leetcode-cn.com/problems/valid-parentheses/
  */
 public class ValidParentheses {
+
+    public static void main(String[] args) {
+        ValidParentheses validParentheses = new ValidParentheses();
+
+        String s = "{{()[]()}}";
+
+        Boolean result = validParentheses.isValid(s);
+
+        System.out.println(result);
+    }
+
+    /**
+     * 左括号放入栈中，被右括号匹配后取出
+     * 时间复杂度：O（ N ）
+     * 空间复杂度：O（ N + ? ），?表示哈希表字符集空间
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        Map<Character, Character> pair =  new HashMap<>();
+        pair.put('(', ')');
+        pair.put('[', ']');
+        pair.put('{', '}');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (pair.containsKey(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            }else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (pair.get(stack.peek()).equals(s.charAt(i))) {
+                    stack.pop();
+                }else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
 }
