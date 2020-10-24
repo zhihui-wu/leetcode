1a7bf678dc9c60713f83bc17e9ed88059804ae7f
zhihui_wu01@163.com <1449553037@qq.com>
2020-10-24 11:21:05 +0800

    FEAT 70. 爬楼梯-wzh/递归

--- src/thought/recursive/ClimbingStairs.java
+++ src/thought/recursive/ClimbingStairs.java
@@ -1,5 +1,9 @@
 package thought.recursive;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description: 70. 爬楼梯
  * @Author: 吴智慧
@@ -7,4 +11,59 @@ package thought.recursive;
  * https://leetcode-cn.com/problems/climbing-stairs/
  */
 public class ClimbingStairs {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+
+//        int n = 3;
+        int n = 45;
+
+
+        /**
+         * wzh:递归
+         */
+        int result = climbingStairs.climbStairs(n);
+
+        /**
+         * todo 解法一：动态规划
+         */
+
+        /**
+         * todo 解法二：遍历迭代
+         */
+
+        System.out.println(result);
+    }
+
+    /**
+     * wzh:递归
+     * 时间复杂度：O（ N ）
+     * 空间复杂度：O（ N ）
+     * 注意：双向队列只存储数组元素下标
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int onePart = 0;
+        int twoPart = 0;
+        if (map.containsKey(n-1)) {
+            onePart = map.get(n-1);
+        }else {
+            onePart = climbStairs(n-1);
+        }
+        if (map.containsKey((n-2))) {
+            twoPart = map.get(n-2);
+        }else {
+            twoPart = climbStairs(n-2);
+
+        }
+        map.put(n, onePart + twoPart);
+        return onePart + twoPart;
+    }
 }
