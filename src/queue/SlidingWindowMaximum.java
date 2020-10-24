472ec6f51276fe7d36f54b781ed3f1cb36db8fdc
zhihui_wu01@163.com <1449553037@qq.com>
2020-10-24 10:50:15 +0800

    FEAT 239. 滑动窗口最大值-解法二：双向队列

--- src/queue/SlidingWindowMaximum.java
+++ src/queue/SlidingWindowMaximum.java
@@ -1,5 +1,7 @@
 package queue;
 
+import java.util.ArrayDeque;
+
 /**
  * @Description: 239. 滑动窗口最大值
  * @Author: 吴智慧
@@ -7,6 +9,7 @@ package queue;
  * https://leetcode-cn.com/problems/sliding-window-maximum/
  */
 public class SlidingWindowMaximum {
+
 
     public static void main(String[] args) {
         SlidingWindowMaximum slidingWindowMaximum = new SlidingWindowMaximum();
@@ -18,17 +21,114 @@ public class SlidingWindowMaximum {
          * 解法一：暴力法（优化）（wzh）
          *
          */
-        int[] result = slidingWindowMaximum.maxSlidingWindow(nums, k);
+//        int[] result = slidingWindowMaximum.maxSlidingWindow(nums, k);
+
+        /**
+         * 解法一：暴力法
+         */
+//        int[] result = slidingWindowMaximum.maxSlidingWindow1(nums, k);
+
+        /**
+         * 解法二：双向队列
+         */
+        int[] result = slidingWindowMaximum.maxSlidingWindow2(nums, k);
+
+        /**
+         * todo 解法三：动态规划
+         */
 
         for (int a : result) {
             System.out.println(a);
         }
     }
 
+
     /**
-     * 暴力法
+     * 双向队列
+     */
+    private ArrayDeque<Integer> deq = new ArrayDeque<>();
+
+    /**
+     * 解法二：双向队列
+     * 时间复杂度：O（ N ）
+     * 空间复杂度：O（ N ）
+     * 注意：双向队列只存储数组元素下标
+     */
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) {
+            return new int[0];
+        }
+        if (k == 1) {
+            return nums;
+        }
+
+        // 初始化双向队列
+        int maxId = 0;
+        for (int i = 0; i < k; i++) {
+            clean_deq(i, k, nums);
+            deq.addLast(i);
+            if (nums[i] > nums[maxId]) {
+                maxId = i;
+            }
+        }
+        int[] output = new int[n - k + 1];
+        output[0] = nums[maxId];
+
+        // 构建输出数组
+        for (int i = k; i < n; i++) {
+            clean_deq(i, k, nums);
+            deq.addLast(i);
+            output[i - k + 1] = nums[deq.getFirst()];
+        }
+        return output;
+    }
+
+    /**
+     * 清除双向队列
+     * @param i 当前需入队的元素下标
+     * @param k 窗口大小，即队列大小
+     * @param nums 数组
+     */
+    public void clean_deq(int i, int k, int[] nums) {
+        // 将滑动窗口最左侧超出窗口范围的元素移出队列
+        if (!deq.isEmpty() && deq.getFirst() == i - k) {
+            deq.removeFirst();
+        }
+        // 以队列从尾到头的方向，移除其中小于当前需入队元素的元素
+        while (!deq.isEmpty() && nums[deq.getLast()] < nums[i]) {
+            deq.removeLast();
+        }
+    }
+
+
+    /**
+     * 解法一：暴力法
      * 时间复杂度：O（ k * N ）
-     * 空间复杂度：O（ k * N ）
+     * 空间复杂度：O（ N - k + 1 ）
+     * 注意，数据量大会超时
+     */
+    public int[] maxSlidingWindow1(int[] nums, int k) {
+        int n = nums.length;
+        if (n * k == 0) {
+            return new int[0];
+        }
+        int[] output = new int[n-k+1];
+        for (int i = 0; i < n - k + 1; i++) {
+            int max = Integer.MIN_VALUE;
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            output[i] = max;
+        }
+        return output;
+    }
+
+
+    /**
+     * 暴力法（优化）（wzh）
+     * 时间复杂度：O（ k * N ）
+     * 空间复杂度：O（ N - k + 1 ）
      */
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums.length < 1 || k < 1) {
