9a8273920a8665949557f07c321b50d23b36054d
zhihui_wu01@163.com <1449553037@qq.com>
2020-11-13 18:08:08 +0800

    FEAT 844. 比较含退格的字符串

--- src/stack/BackspaceStringCompare.java
+++ src/stack/BackspaceStringCompare.java
@@ -1,5 +1,8 @@
 package stack;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Description: 844. 比较含退格的字符串
  * @Author: 吴智慧
@@ -7,4 +10,64 @@ package stack;
  * https://leetcode-cn.com/problems/backspace-string-compare/
  */
 public class BackspaceStringCompare {
+
+    public static void main(String[] args) {
+
+        BackspaceStringCompare backspaceStringCompare = new BackspaceStringCompare();
+//        String S = "ab#c";
+//        String T = "ad#c";
+        String S = "abcd";
+        String T = "bbcd";
+
+        // 计算退格后的最终字符串，进行比对
+        boolean result = backspaceStringCompare.backspaceCompare(S, T);
+        System.out.println(result);
+
+    }
+
+    public boolean backspaceCompare(String S, String T) {
+
+        Deque<Character> sStack = new LinkedList<>();
+        for (char c : S.toCharArray()) {
+            if (c != '#') {
+                sStack.push(c);
+            }
+            if (c == '#') {
+                if (!sStack.isEmpty()) {
+                    sStack.pop();
+                }
+            }
+        }
+
+        Deque<Character> tStack = new LinkedList<>();
+        for (char c : T.toCharArray()) {
+            if (c != '#') {
+                tStack.push(c);
+            }
+            if (c == '#') {
+                if (!tStack.isEmpty()) {
+                    tStack.pop();
+                }
+            }
+        }
+
+        if (sStack.size() != tStack.size()) {
+            return false;
+        }
+
+        if (sStack.isEmpty() && tStack.isEmpty()) {
+            return true;
+        }
+
+        int size = sStack.size();
+        boolean result = true;
+
+        for (int i = 0; i < size; i++) {
+            if (!sStack.pop().equals(tStack.pop())) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 }
