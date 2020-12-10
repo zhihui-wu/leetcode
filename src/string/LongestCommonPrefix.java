bbd1e0c2029e72d621a7a14a37467ac8e7de52fd
zhihui_wu01@163.com <1449553037@qq.com>
2020-12-10 19:49:28 +0800

    FEAT 14. 最长公共前缀

---
+++ src/string/LongestCommonPrefix.java
@@ -0,0 +1,53 @@
+package string;
+
+/**
+ * @Description: 14. 最长公共前缀
+ * @Author: 吴智慧
+ * @Date: 2020/12/4 12:26
+ * https://leetcode-cn.com/problems/longest-common-prefix/
+ */
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
+        String[] strs = new String[]{"flower","flow","flight"};
+
+        String result = longestCommonPrefix.longestCommonPrefix(strs);
+
+        System.out.println(result);
+
+    }
+
+
+    /**
+     * 时间复杂度：O（ n * m ）
+     * 空间复杂度：O（ 1 ）
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length < 1){
+            return "";
+        }
+        if (strs.length == 1){
+            return strs[0];
+        }
+        StringBuilder result = new StringBuilder("");
+        char temp = ' ';
+        int i = 0;
+        while (true) {
+            for (int j = 0; j < strs.length; j++) {
+                if (i >= strs[j].length() || (temp != ' ' && temp != strs[j].charAt(i))) {
+                    return result.toString();
+                }
+                if (temp == ' '){
+                    temp = strs[j].charAt(i);
+                }
+            }
+            result = result.append(temp);
+            temp = ' ';
+            i++;
+        }
+    }
+
+}
