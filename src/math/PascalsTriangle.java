8c701dc55f4dc7c4542156db97243fb592a2a3e9
zhihui_wu01@163.com <1449553037@qq.com>
2021-03-05 09:46:32 +0800

    FEAT 118. 杨辉三角

--- src/math/PascalsTriangle.java
+++ src/math/PascalsTriangle.java
@@ -48,4 +48,27 @@ public class PascalsTriangle {
     }
 
 
+    /**
+     * 时间复杂度：O（ n ^ 2 ）
+     * 空间复杂度：O（ n ^ 2 ）
+     */
+    public List<List<Integer>> generate1(int numRows) {
+
+        List<List<Integer>> result = new ArrayList();
+        for(int i = 0; i < numRows; i++) {
+            List<Integer> temp = new ArrayList();
+            for(int j = 0; j <= i; j++) {
+                if(j == 0 || j == i) {
+                    temp.add(1);
+                }else {
+                    temp.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+                }
+            }
+            result.add(temp);
+        }
+        return result;
+
+    }
+
+
 }
