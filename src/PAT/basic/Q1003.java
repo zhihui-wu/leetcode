4d78d6295d2104e11f97eb97bb6195a4798dddd1
zhihui_wu01@163.com <1449553037@qq.com>
2021-05-26 16:01:59 +0800

    FEAT 1003 我要通过！

--- src/PAT/basic/Q1003.java
+++ src/PAT/basic/Q1003.java
@@ -1,6 +1,8 @@
 package PAT.basic;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * @Description: 1003 我要通过！
@@ -13,14 +15,55 @@ public class Q1003 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
-        for (int i = 0; i < a; i++) {
+//         System.out.println(a);
+        for (int i = 0; i < a + 1; i++) {
             String s = in.nextLine();
-            doConfirm(s);
+            if (s.length() > 0){
+                System.out.print(doConfirm(s) ? "YES" : "NO");
+                if (i < a) {
+                    System.out.println("");
+                }
+            }
         }
     }
 
-    private static void doConfirm(String s) {
-
+    /**
+     * 要看懂题目的意思，有隐含的规则
+     */
+    private static boolean doConfirm(String s) {
+//         System.out.println(s);
+        int pIndex = Integer.MIN_VALUE;
+        int pCount = 0;
+        int tIndex = Integer.MAX_VALUE;
+        int tCount = 0;
+        Set<Character> set = new HashSet<>();
+        set.add('P');
+        set.add('A');
+        set.add('T');
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'P') {
+                pIndex = i;
+                pCount++;
+            }
+            if (s.charAt(i) == 'T') {
+                tIndex = i;
+                tCount++;
+            }
+            if (!set.contains(s.charAt(i)) || pIndex >= tIndex) {
+                return false;
+            }
+        }
+        if (pCount == 1 && tCount == 1) {
+//             System.out.println("a:"+ s.substring(0, pIndex) + ", b:" + s.substring(pIndex+1, tIndex) + ", c:" +s.substring(tIndex+1, s.length()));
+            String  a = s.substring(0, pIndex);
+            String  b = s.substring(pIndex+1, tIndex);
+            String  c = s.substring(tIndex+1, s.length());
+            if (b.length() < 1) {
+                return false;
+            }
+            return c.length() == a.length() * b.length();
+        }
+        return false;
     }
 
 }
